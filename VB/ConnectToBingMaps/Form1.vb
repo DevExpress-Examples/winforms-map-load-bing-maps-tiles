1845e8b0583a42454579555381a473406534b366
DevExpressExampleBot <fakeEmail>
2022-09-01 12:31:14 +0400

    Source auto update [skip ci]

--- VB/ConnectToBingMaps/Form1.vb
+++ VB/ConnectToBingMaps/Form1.vb
@@ -1,34 +1,31 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
 Imports System.Windows.Forms
 Imports DevExpress.XtraMap
 
 Namespace ConnectToBingMaps
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
 
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-			' Create a map control.
-			Dim map As New MapControl()
+    Public Partial Class Form1
+        Inherits Form
 
-			' Specify the map position on the form.           
-			map.Dock = DockStyle.Fill
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-			' Add the map control to the window.
-			Me.Controls.Add(map)
-
-			' Create an image tiles layer and add it to the map.
-			Dim tilesLayer As New ImageTilesLayer()
-			map.Layers.Add(tilesLayer)
-
-			' Create a Bing data provider and specify the Bing key.
-			Dim bingProvider As New BingMapDataProvider()
-			tilesLayer.DataProvider = bingProvider
-			bingProvider.BingKey = "INSERT YOUR BING KEY HERE"
-			bingProvider.Kind = BingMapKind.Area
-		End Sub
-	End Class
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+            ' Create a map control.
+            Dim map As MapControl = New MapControl()
+            ' Specify the map position on the form.           
+            map.Dock = DockStyle.Fill
+            ' Add the map control to the window.
+            Me.Controls.Add(map)
+            ' Create an image tiles layer and add it to the map.
+            Dim tilesLayer As ImageTilesLayer = New ImageTilesLayer()
+            map.Layers.Add(tilesLayer)
+            ' Create a Bing data provider and specify the Bing key.
+            Dim bingProvider As BingMapDataProvider = New BingMapDataProvider()
+            tilesLayer.DataProvider = bingProvider
+            bingProvider.BingKey = "INSERT YOUR BING KEY HERE"
+            bingProvider.Kind = BingMapKind.Area
+        End Sub
+    End Class
 End Namespace
